62d5b4e437bbbacf57f75c2243583d4d1b2fbb09
Alison Appling <aappling@usgs.gov>
2015-04-28 11:06:53 -0500

    adding & testing calc_depth()

---
+++ R/calc_depth.R
@@ -0,0 +1,41 @@
+#' Estimate depth from discharge and hydraulic geometry coefficients
+#' 
+#' Uses the relationship \eqn{d=c*Q^f} (parameter names and definitions as in 
+#' Leopold and Maddock, 1953; default values for c and f as in Raymond et al.
+#' 2012)
+#' 
+#' @param Q discharge (m^3 s^-1)
+#' @param c coefficient representing depth at unit discharge (usually m)
+#' @param f exponent in depth-discharge relation (unitless)
+#' @return d, stream depth, in the same units as c
+#' @examples
+#' calc_depth(Q=1:10)
+#' calc_depth(Q=1:10, f=0.4)
+#' library(unitted)
+#' calc_depth(Q=u(1:10, "m^3 s^-1"), c=u(40,"cm"))
+#' calc_depth(Q=u(1:10, "m^3 s^-1"), f=u(0.36))
+#' @importFrom unitted u v verify_units
+#' @export
+calc_depth <- function(Q, c=u(0.409,"m"), f=u(0.294,"")) {
+
+  with.units <- is.unitted(Q) || (if(!missing(c)) is.unitted(c) else FALSE) || (if(!missing(f)) is.unitted(f) else FALSE)
+
+  if(with.units) {
+    # if any units are set, they all must be set and must be correct
+    verify_units(Q, "m^3 s^-1")
+    if(!(get_units(c) %in% c("m","cm","mm","ft","in"))) warning("c has unknown depth units (",get_units(c),")")
+    verify_units(f, "")
+  } else {
+    # if no units are explicitly set, then make sure c and f aren't using their unitted defaults
+    c <- v(c)
+    f <- v(f)
+  }
+
+  # The exponential form (below) is equivalent to this log-log form:
+  #   d <- exp(log(v(c)) + log(v(Q)) * f)
+  #   if(with.units) d <- u(d, get_units(c))
+    
+  # Do the calculation, overriding the Q units (if any) for this empirical
+  # equation. If c is unitted, units will be carried through.
+  c * v(Q) ^ f
+}
