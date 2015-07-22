e853fb6b0e83dc2115b70f28ed9a4c6256384e45
Alison Appling <aappling@usgs.gov>
2015-07-22 07:31:56 -0500

    resolve #66 rename metab_bayes_simple

--- R/metab_bayes.R
+++ R/metab_bayes.R
@@ -11,38 +11,38 @@ NULL
 #'   as the default. See \code{\link{mm_data}} for a full data description.
 #' @param info Any metadata you would like to package within the metabolism 
 #'   model.
-#' @inheritParams runjags_bayes_simple
+#' @inheritParams runjags_bayes
 #' @inheritParams mm_is_valid_day
-#' @return A metab_bayes_simple object containing the fitted model.
+#' @return A metab_bayes object containing the fitted model.
 #' @examples
 #' \dontrun{
-#'  metab_bayes_simple(data=data.frame(empty="shouldbreak"))
+#'  metab_bayes(data=data.frame(empty="shouldbreak"))
 #' }
 #' @export
 #' @family metab_model
-metab_bayes_simple <- function(
-  data=mm_data(local.time, DO.obs, DO.sat, depth, temp.water, light), # args for bayes_simple_1ply
-  info=NULL, # args for new("metab_bayes_simple")
+metab_bayes <- function(
+  data=mm_data(local.time, DO.obs, DO.sat, depth, temp.water, light), # args for bayes_1ply
+  info=NULL, # args for new("metab_bayes")
   #fit_distribs=c("GPP","ER","K600"), fit_autocor=c("GPP","ER","K600"), fit_resids=c("K600"), fit_function_K600=c("splineKvT","lmKvQ"),
-  maxCores=4, adaptSteps=10, burnInSteps=40, numSavedSteps=400, thinSteps=1, # args for runjags_bayes_simple
+  maxCores=4, adaptSteps=10, burnInSteps=40, numSavedSteps=400, thinSteps=1, # args for runjags_bayes
   tests=c('full_day', 'even_timesteps', 'complete_data'), day_start=-1.5, day_end=30 # args for mm_is_valid_day, mm_model_by_ply
 ) {
   
   # Check data for correct column names & units
-  data <- mm_validate_data(data, "metab_bayes_simple")
+  data <- mm_validate_data(data, "metab_bayes")
   
   # model the data, splitting into overlapping 31.5-hr 'plys' for each date
   bayes.all <- mm_model_by_ply(
-    data, bayes_simple_1ply, # for mm_model_by_ply
+    data, bayes_1ply, # for mm_model_by_ply
     day_start=day_start, day_end=day_end, # for mm_model_by_ply and mm_is_valid_day
     tests=tests, # for mm_is_valid_day
-    maxCores=maxCores, adaptSteps=adaptSteps, burnInSteps=burnInSteps, numSavedSteps=numSavedSteps, thinSteps=thinSteps) # for bayes_simple_1ply
+    maxCores=maxCores, adaptSteps=adaptSteps, burnInSteps=burnInSteps, numSavedSteps=numSavedSteps, thinSteps=thinSteps) # for bayes_1ply
   
   # Package and return results
-  new("metab_bayes_simple", 
+  new("metab_bayes", 
       info=info,
       fit=bayes.all,
-      # metab_bayes_simple always uses the equivalent of calc_DO_mod
+      # metab_bayes always uses the equivalent of calc_DO_mod
       args=list(calc_DO_fun=calc_DO_mod, day_start=day_start, day_end=day_end,
                 maxCores=maxCores, adaptSteps=adaptSteps, burnInSteps=burnInSteps, 
                 numSavedSteps=numSavedSteps, thinSteps=thinSteps),
@@ -55,19 +55,19 @@ metab_bayes_simple <- function(
 
 #' Make daily metabolism estimates from input parameters
 #' 
-#' Called from metab_bayes_simple().
+#' Called from metab_bayes().
 #' 
 #' @param data_ply data.frame of the form \code{mm_data(local.time, DO.obs, DO.sat,
 #'   depth, temp.water, light)} and containing data for just one estimation-day
 #'   (this may be >24 hours but only yields estimates for one 24-hour period)
 #' @param calc_DO_fun the function to use to build DO estimates from GPP, ER,
 #'   etc. default is calc_DO_mod, but could also be calc_DO_mod_by_diff
-#' @inheritParams runjags_bayes_simple
+#' @inheritParams runjags_bayes
 #' @param ... additional args passed from mm_model_by_ply and ignored here
 #' @return data.frame of estimates and \code{\link[stats]{nlm}} model 
 #'   diagnostics
 #' @keywords internal
-bayes_simple_1ply <- function(data_ply, maxCores=4, adaptSteps=1000, burnInSteps=4000, numSavedSteps=40000, thinSteps=1, ...) {
+bayes_1ply <- function(data_ply, maxCores=4, adaptSteps=1000, burnInSteps=4000, numSavedSteps=40000, thinSteps=1, ...) {
   
   # Provide ability to skip a poorly-formatted day for calculating 
   # metabolism, without breaking the whole loop. Just collect 
@@ -80,8 +80,8 @@ bayes_simple_1ply <- function(data_ply, maxCores=4, adaptSteps=1000, burnInSteps
     bayes.1d <- withCallingHandlers(
       tryCatch({
         # first: try to run the bayes fitting function
-        data.list <- prepjags_bayes_simple(data_ply)
-        runjags_bayes_simple(dataList=data.list, maxCores=maxCores, adaptSteps=adaptSteps, 
+        data.list <- prepjags_bayes(data_ply)
+        runjags_bayes(dataList=data.list, maxCores=maxCores, adaptSteps=adaptSteps, 
                              burnInSteps=burnInSteps, numSavedSteps=numSavedSteps, thinSteps=thinSteps)
       }, error=function(err) {
         # on error: give up, remembering error. dummy values provided below
@@ -124,7 +124,7 @@ bayes_simple_1ply <- function(data_ply, maxCores=4, adaptSteps=1000, burnInSteps
 #'   return priors rather than posteriors?
 #' @return list of data for input to runjags
 #' @export
-prepjags_bayes_simple <- function(data_ply, priors=FALSE) {
+prepjags_bayes <- function(data_ply, priors=FALSE) {
   
   #Useful info for setting JAGS data
   date <- names(which.max(table(as.Date(data_ply$local.time))))
@@ -177,7 +177,7 @@ prepjags_bayes_simple <- function(data_ply, priors=FALSE) {
 #' @import runjags
 #' @import parallel
 #' @export
-runjags_bayes_simple <- function(dataList, maxCores=4, adaptSteps=1000, burnInSteps=4000, numSavedSteps=40000, thinSteps=1) {
+runjags_bayes <- function(dataList, maxCores=4, adaptSteps=1000, burnInSteps=4000, numSavedSteps=40000, thinSteps=1) {
   
   initsFun <- function(chain) {
     list(.RNG.name=
@@ -197,7 +197,7 @@ runjags_bayes_simple <- function(dataList, maxCores=4, adaptSteps=1000, burnInSt
   
   runJagsOut <- run.jags(
     method=c("rjags","parallel","snow")[2],
-    model=system.file("extdata/metab_bayes_simple_jags.txt", package="streamMetabolizer"),
+    model=system.file("extdata/metab_bayes_jags.txt", package="streamMetabolizer"),
     monitor=parameters,
     data=dataList,
     inits=initsFun,
@@ -219,17 +219,17 @@ runjags_bayes_simple <- function(dataList, maxCores=4, adaptSteps=1000, burnInSt
 
 
 
-#### metab_bayes_simple class ####
+#### metab_bayes class ####
 
 #' Metabolism model fitted by maximum likelihood estimation
 #' 
-#' \code{metab_bayes_simple} models use non-linear minimization of the negative log
+#' \code{metab_bayes} models use non-linear minimization of the negative log
 #' likelihood to fit values of GPP, ER, and K for a given DO curve.
 #' 
-#' @exportClass metab_bayes_simple
+#' @exportClass metab_bayes
 #' @family metab.model.classes
 setClass(
-  "metab_bayes_simple", 
+  "metab_bayes", 
   contains="metab_model"
 )
 
