3af362b888471206ffcf902c5f108238cbe68883
Alison Appling <aappling@usgs.gov>
2015-10-15 11:48:49 -0400

    use underscore_seps for bayes (stan requires)

--- R/specs_bayes_jags_nopool_obserr.R
+++ R/specs_bayes_jags_nopool_obserr.R
@@ -6,7 +6,7 @@
 #'   
 #' @inheritParams specs_bayes_jags_nopool_procobserr
 #' @inheritParams prepdata_bayes
-#' @inheritParams runjags_bayes
+#' @inheritParams mcmc_bayes
 #'   
 #' @export
 specs_bayes_jags_nopool_obserr <- function(
@@ -17,26 +17,27 @@ specs_bayes_jags_nopool_obserr <- function(
   bayes_software = 'jags',
   
   # hyperparameters
-  GPP.daily.mu = 10,
-  GPP.daily.sigma = 10,
-  ER.daily.mu = -10,
-  ER.daily.sigma = 10,
-  K600.daily.mu = 10,
-  K600.daily.sigma = 10,
+  GPP_daily_mu = 10,
+  GPP_daily_sigma = 10,
+  ER_daily_mu = -10,
+  ER_daily_sigma = 10,
+  K600_daily_mu = 10,
+  K600_daily_sigma = 10,
   
-  err.obs.sigma.min = 0,
-  err.obs.sigma.max = 0.5,
+  err_obs_sigma_min = 0,
+  err_obs_sigma_max = 0.5,
   
   # inheritParams prepdata_bayes
   priors = FALSE,
   
-  # inheritParams runjags_bayes
-  params_out = c("GPP.daily", "ER.daily", "K600.daily", "err.obs.sigma"),
+  # inheritParams mcmc_bayes
+  params_out = c("GPP_daily", "ER_daily", "K600_daily", "err_obs_sigma"),
   max_cores = 4, 
   adapt_steps = 100, 
   burnin_steps = 40, 
   num_saved_steps = 400, 
-  thin_steps = 1
+  thin_steps = 1,
+  verbose = TRUE
   
 ) {
   
@@ -45,15 +46,15 @@ specs_bayes_jags_nopool_obserr <- function(
     bayes_fun = bayes_fun,
     bayes_software = bayes_software,
     
-    GPP.daily.mu = GPP.daily.mu,
-    GPP.daily.sigma = GPP.daily.sigma,
-    ER.daily.mu = ER.daily.mu,
-    ER.daily.sigma = ER.daily.sigma,
-    K600.daily.mu = K600.daily.mu,
-    K600.daily.sigma = K600.daily.sigma,
+    GPP_daily_mu = GPP_daily_mu,
+    GPP_daily_sigma = GPP_daily_sigma,
+    ER_daily_mu = ER_daily_mu,
+    ER_daily_sigma = ER_daily_sigma,
+    K600_daily_mu = K600_daily_mu,
+    K600_daily_sigma = K600_daily_sigma,
     
-    err.obs.sigma.min = err.obs.sigma.min,
-    err.obs.sigma.max = err.obs.sigma.max,
+    err_obs_sigma_min = err_obs_sigma_min,
+    err_obs_sigma_max = err_obs_sigma_max,
     
     priors = priors,
     
@@ -62,7 +63,8 @@ specs_bayes_jags_nopool_obserr <- function(
     adapt_steps = adapt_steps, 
     burnin_steps = burnin_steps, 
     num_saved_steps = num_saved_steps, 
-    thin_steps = thin_steps
+    thin_steps = thin_steps,
+    verbose = verbose
   )
   
 }
--- R/specs_bayes_jags_nopool_procobserr.R
+++ R/specs_bayes_jags_nopool_procobserr.R
@@ -23,33 +23,33 @@
 #' @param bayes_software character in \code{c('jags','stan')} indicating the 
 #'   software package to use for the MCMC process
 #'   
-#' @param GPP.daily.mu The mean of a dnorm distribution for GPP.daily, the daily
+#' @param GPP_daily_mu The mean of a dnorm distribution for GPP_daily, the daily
 #'   rate of gross primary production
-#' @param GPP.daily.sigma The standard deviation of a dnorm distribution for 
-#'   GPP.daily, the daily rate of gross primary production
-#' @param ER.daily.mu The mean of a dnorm distribution for ER.daily, the daily 
+#' @param GPP_daily_sigma The standard deviation of a dnorm distribution for 
+#'   GPP_daily, the daily rate of gross primary production
+#' @param ER_daily_mu The mean of a dnorm distribution for ER_daily, the daily 
 #'   rate of ecosystem respiration
-#' @param ER.daily.sigma The standard deviation of a dnorm distribution for 
-#'   ER.daily, the daily rate of ecosystem respiration
-#' @param K600.daily.mu The mean of a dnorm distribution for K600.daily, the 
+#' @param ER_daily_sigma The standard deviation of a dnorm distribution for 
+#'   ER_daily, the daily rate of ecosystem respiration
+#' @param K600_daily_mu The mean of a dnorm distribution for K600_daily, the 
 #'   daily rate of reaeration
-#' @param K600.daily.sigma The standard deviation of a dnorm distribution for 
-#'   K600.daily, the daily rate of reaeration
-#' @param err.proc.phi.min The lower bound on a dunif distribution for 
-#'   err.proc.phi, the process error autocorrelation coefficient
-#' @param err.proc.phi.max The upper bound on a dunif distribution for 
-#'   err.proc.phi, the process error autocorrelation coefficient
-#' @param err.proc.sigma.min The lower bound on a dunif distribution for 
-#'   err.proc.sigma, the standard deviation of the process error
-#' @param err.proc.sigma.max The upper bound on a dunif distribution for 
-#'   err.proc.sigma, the standard deviation of the process error
-#' @param err.obs.sigma.min The lower bound on a dunif distribution for 
-#'   err.obs.sigma, the standard deviation of the observation error
-#' @param err.obs.sigma.max The upper bound on a dunif distribution for 
-#'   err.obs.sigma, the standard deviation of the observation error
+#' @param K600_daily_sigma The standard deviation of a dnorm distribution for 
+#'   K600_daily, the daily rate of reaeration
+#' @param err_proc_phi_min The lower bound on a dunif distribution for 
+#'   err_proc_phi, the process error autocorrelation coefficient
+#' @param err_proc_phi_max The upper bound on a dunif distribution for 
+#'   err_proc_phi, the process error autocorrelation coefficient
+#' @param err_proc_sigma_min The lower bound on a dunif distribution for 
+#'   err_proc_sigma, the standard deviation of the process error
+#' @param err_proc_sigma_max The upper bound on a dunif distribution for 
+#'   err_proc_sigma, the standard deviation of the process error
+#' @param err_obs_sigma_min The lower bound on a dunif distribution for 
+#'   err_obs_sigma, the standard deviation of the observation error
+#' @param err_obs_sigma_max The upper bound on a dunif distribution for 
+#'   err_obs_sigma, the standard deviation of the observation error
 #'   
 #' @inheritParams prepdata_bayes
-#' @inheritParams runjags_bayes
+#' @inheritParams mcmc_bayes
 #'   
 #' @export
 specs_bayes_jags_nopool_procobserr <- function(
@@ -60,30 +60,31 @@ specs_bayes_jags_nopool_procobserr <- function(
   bayes_software = 'jags',
   
   # hyperparameters
-  GPP.daily.mu = 10,
-  GPP.daily.sigma = 10,
-  ER.daily.mu = -10,
-  ER.daily.sigma = 10,
-  K600.daily.mu = 10,
-  K600.daily.sigma = 10,
+  GPP_daily_mu = 10,
+  GPP_daily_sigma = 10,
+  ER_daily_mu = -10,
+  ER_daily_sigma = 10,
+  K600_daily_mu = 10,
+  K600_daily_sigma = 10,
   
-  err.proc.phi.min = 0,
-  err.proc.phi.max = 1,
-  err.proc.sigma.min = 0,
-  err.proc.sigma.max = 0.0005,
-  err.obs.sigma.min = 0,
-  err.obs.sigma.max = 0.5,
+  err_proc_phi_min = 0,
+  err_proc_phi_max = 1,
+  err_proc_sigma_min = 0,
+  err_proc_sigma_max = 0.0005,
+  err_obs_sigma_min = 0,
+  err_obs_sigma_max = 0.5,
   
   # inheritParams prepdata_bayes
   priors = FALSE,
   
-  # inheritParams runjags_bayes
-  params_out = c("GPP.daily", "ER.daily", "K600.daily", "err.obs.sigma", "err.proc.sigma", "err.proc.phi"),
+  # inheritParams mcmc_bayes
+  params_out = c("GPP_daily", "ER_daily", "K600_daily", "err_obs_sigma", "err_proc_sigma", "err_proc_phi"),
   max_cores = 4, 
   adapt_steps = 100, 
   burnin_steps = 40, 
   num_saved_steps = 400, 
-  thin_steps = 1
+  thin_steps = 1,
+  verbose = TRUE
   
 ) {
   
@@ -93,19 +94,19 @@ specs_bayes_jags_nopool_procobserr <- function(
     bayes_fun = bayes_fun,
     bayes_software = bayes_software,
     
-    GPP.daily.mu = GPP.daily.mu,
-    GPP.daily.sigma = GPP.daily.sigma,
-    ER.daily.mu = ER.daily.mu,
-    ER.daily.sigma = ER.daily.sigma,
-    K600.daily.mu = K600.daily.mu,
-    K600.daily.sigma = K600.daily.sigma,
+    GPP_daily_mu = GPP_daily_mu,
+    GPP_daily_sigma = GPP_daily_sigma,
+    ER_daily_mu = ER_daily_mu,
+    ER_daily_sigma = ER_daily_sigma,
+    K600_daily_mu = K600_daily_mu,
+    K600_daily_sigma = K600_daily_sigma,
     
-    err.proc.phi.min = err.proc.phi.min,
-    err.proc.phi.max = err.proc.phi.max,
-    err.proc.sigma.min = err.proc.sigma.min,
-    err.proc.sigma.max = err.proc.sigma.max,
-    err.obs.sigma.min = err.obs.sigma.min,
-    err.obs.sigma.max = err.obs.sigma.max,
+    err_proc_phi_min = err_proc_phi_min,
+    err_proc_phi_max = err_proc_phi_max,
+    err_proc_sigma_min = err_proc_sigma_min,
+    err_proc_sigma_max = err_proc_sigma_max,
+    err_obs_sigma_min = err_obs_sigma_min,
+    err_obs_sigma_max = err_obs_sigma_max,
     
     priors = priors,
     
@@ -114,7 +115,8 @@ specs_bayes_jags_nopool_procobserr <- function(
     adapt_steps = adapt_steps, 
     burnin_steps = burnin_steps, 
     num_saved_steps = num_saved_steps, 
-    thin_steps = thin_steps
+    thin_steps = thin_steps,
+    verbose = verbose
     
   )
   
