4e3e0a01b215b5eb83536f379bdd5f2741ea3888
Alison Appling <aappling@usgs.gov>
2015-11-24 11:58:36 -0600

    better DO diagnostic plotting

--- R/plot_DO_preds.R
+++ R/plot_DO_preds.R
@@ -4,33 +4,47 @@
 #' 
 #' @param DO_preds a data.frame of predictions such as that returned by 
 #'   predict_DO()
-#' @param plot_as character. Should the plot display predicted & observed values
-#'   in concentration (conc) or as percent of saturation (pctsat)? The default
+#' @param y_var character. Should the plot display predicted & observed values 
+#'   in concentration (conc) or as percent of saturation (pctsat)? The default 
 #'   is to plot both.
+#' @param style character indicating which graphics package to use
+#' @param y_lim list of named vectors, each of which has length 2 and is 
+#'   numeric. NA within a vector indicates that the data range should be used. 
+#'   for ggplot2, y_lim is only used to exclude values outside that range and is
+#'   ignored if the data span a narrower range
 #' @examples 
 #' \dontrun{
 #' mm <- metab_night(v(french))
 #' plot_DO_preds(predict_DO(mm)[1:360,])
 #' }
-#' @import ggplot2
 #' @import dplyr
+#' @importFrom unitted v
 #' @export
-plot_DO_preds <- function(DO_preds, plot_as=c('conc','pctsat','ddodt')) {
+plot_DO_preds <- function(DO_preds, y_var=c('conc','pctsat','ddodt'), 
+                          style=c('ggplot2','dygraphs'),
+                          y_lim=list(conc=c(NA,NA), pctsat=c(NA,NA), ddodt=c(NA,NA))) {
   
-  plot_as <- match.arg(plot_as, several.ok=TRUE)
+  style <- match.arg(style)
+  y_var <- match.arg(y_var, several.ok=TRUE)
+  
+  params <- list(
+    xlab='Local time',
+    ylab='Predictions (lines) and observations (points)',
+    colors=list(conc=c('#A64B00','#FF7400'), pctsat=c('#007929','#23BC47'), ddodt=c('#05326D','#4282D3'))
+  )
   
   DO.obs <- DO.mod <- DO.sat <- '.dplyr.var'
   DO_preds_conc <- mutate(
-    DO_preds, as='conc', var='DO (mg/L)', col='red4', lab='DO (mg/L)',
+    DO_preds, as='conc', var='DO (mg/L)', col1=params$colors$conc[1], col2=params$colors$conc[2], lab='DO (mg/L)',
     mod=DO.mod, 
     obs=DO.obs)
   DO_preds_pctsat <- mutate(
-    DO_preds, as='pctsat', var='DO (% sat)', col='forestgreen', lab='DO (% sat)',
+    DO_preds, as='pctsat', var='DO (% sat)', col1=params$colors$pctsat[1], col2=params$colors$pctsat[2], lab='DO (% sat)',
     mod=100*DO.mod/DO.sat, 
     obs=100*DO.obs/DO.sat)
   DO_preds_ddodt <- 
     mutate(
-      DO_preds[-1,], as='ddodt', var='dDO/dt (mg/L/d)', col='navy', lab='dDO/dt~(mg~L^-1~d^-1)',
+      DO_preds[-1,], as='ddodt', var='dDO/dt (mg/L/d)', col1=params$colors$ddodt[1], col2=params$colors$ddodt[2], lab='dDO/dt~(mg~L^-1~d^-1)',
       mod = diff(DO_preds$DO.mod)/as.numeric(diff(DO_preds$local.time), units="days"),
       obs = diff(DO_preds$DO.obs)/as.numeric(diff(DO_preds$local.time), units="days")) %>%
     mutate(
@@ -38,15 +52,82 @@ plot_DO_preds <- function(DO_preds, plot_as=c('conc','pctsat','ddodt')) {
       obs = ifelse(diff(DO_preds$local.date)==0, obs, NA))
   
   DO_preds_all <- bind_rows(DO_preds_conc, DO_preds_pctsat, DO_preds_ddodt) %>%
-    mutate(var=ordered(var, c(conc='DO (mg/L)', pctsat='DO (% sat)', ddodt='dDO/dt (mg/L/d)')[plot_as]))
+    mutate(var=ordered(var, c(conc='DO (mg/L)', pctsat='DO (% sat)', ddodt='dDO/dt (mg/L/d)')[y_var]))
   
-  local.time <- mod <- local.date <- col <- obs <- '.ggplot.var'
-  g <- ggplot(v(DO_preds_all)[DO_preds_all$as %in% plot_as,], aes(x=local.time, group=local.date, color=col)) +
-    geom_line(aes(y=mod), size=0.8) +
-    geom_point(aes(y=obs), alpha=0.3) +
-    scale_color_identity(guide='none') +
-    theme_bw() + facet_grid(var ~ ., scales="free_y") + 
-    xlab('Local time') + ylab("Predictions (lines) and observations (points)")
-  
-  suppressWarnings(print(g))
+  switch(
+    style,
+    'ggplot2' = {
+      if(!requireNamespace("ggplot2", quietly=TRUE))
+        stop("call install.packages('ggplot2') before plotting with style='ggplot2'")
+      
+      . <- local.time <- mod <- local.date <- col1 <- col2 <- obs <- '.ggplot.var'
+      preds_ggplot <- v(DO_preds_all) %>%
+        filter(as %in% y_var)
+      if('conc' %in% names(y_lim)) {
+        lim <- y_lim[['conc']][1]; if(!is.na(lim)) preds_ggplot <- filter(preds_ggplot, as != 'conc' | (mod >= lim & obs >= lim))
+        lim <- y_lim[['conc']][2]; if(!is.na(lim)) preds_ggplot <- filter(preds_ggplot, as != 'conc' | (mod <= lim & obs <= lim))
+      }
+      if('pctsat' %in% names(y_lim)) {
+        lim <- y_lim[['pctsat']][1]; if(!is.na(lim)) preds_ggplot <- filter(preds_ggplot, as != 'pctsat' | (mod >= lim & obs >= lim))
+        lim <- y_lim[['pctsat']][2]; if(!is.na(lim)) preds_ggplot <- filter(preds_ggplot, as != 'pctsat' | (mod <= lim & obs <= lim))
+      }
+      if('ddodt' %in% names(y_lim)) {
+        lim <- y_lim[['ddodt']][1]; if(!is.na(lim)) preds_ggplot <- filter(preds_ggplot, as != 'ddodt' | (mod >= lim & obs >= lim))
+        lim <- y_lim[['ddodt']][2]; if(!is.na(lim)) preds_ggplot <- filter(preds_ggplot, as != 'ddodt' | (mod <= lim & obs <= lim))
+      }
+      g <- ggplot2::ggplot(preds_ggplot, ggplot2::aes(x=local.time, group=local.date)) +
+        ggplot2::geom_line(ggplot2::aes(y=mod, color=col1), size=0.8) +
+        ggplot2::geom_point(ggplot2::aes(y=obs, color=col2), alpha=0.6) +
+        ggplot2::scale_color_identity(guide='none') +
+        ggplot2::theme_bw() + 
+        ggplot2::facet_grid(var ~ ., scales="free_y") + 
+        ggplot2::xlab(params$xlab) + ggplot2::ylab(params$ylab)
+      
+      suppressWarnings(print(g))
+    },
+    'dygraphs' = {
+      if(!requireNamespace("dygraphs", quietly=TRUE))
+        stop("call install.packages(dygraphs') before plotting with style='dygraphs'")
+      if(!requireNamespace("xts", quietly=TRUE))
+        stop("call install.packages(dygraphs') before plotting with style='dygraphs'")
+      
+      . <- '.dplyr.var'
+      preds_xts <- v(DO_preds_all) %>%
+        filter(as %in% y_var) %>%
+        arrange(local.time) %>%
+        group_by(local.date) %>%
+        do(., {
+          out <- .[c(seq_len(nrow(.)),nrow(.)),]
+          out[nrow(.)+1,c('mod','obs')] <- NA
+          out
+        }) %>%
+        ungroup()
+      
+      prep_dygraph <- function(y_var) { 
+        preds_xts %>% 
+          filter(as==y_var) %>% 
+          select(mod,obs,local.time) %>%
+          #setNames(c(paste0(y_var,' mod'), paste0(y_var,' obs'), 'local.time')) %>%
+          mutate(local.time=lubridate::force_tz(local.time, 'UTC')) %>%
+          xts::xts(x=select(., -local.time), order.by=.$local.time) 
+      }
+      if(length(y_var) > 1) warning("can only plot one dygraph y_var at a time for now; plotting y_vars in succession")
+      sapply(y_var, function(yvar) {
+        y_var_long <- preds_xts %>% filter(as==yvar) %>% slice(1) %>% .[['var']] %>% as.character()
+        y_var_col <- params$colors[[yvar]]
+        dat <- prep_dygraph(yvar)
+        ymin <- max(c(min(c(dat[,1], dat[,2]), na.rm=TRUE), y_lim[[yvar]][1]), na.rm=TRUE)
+        ymax <- min(c(max(c(dat[,1], dat[,2]), na.rm=TRUE), y_lim[[yvar]][2]), na.rm=TRUE)
+        dygraphs::dygraph(dat, xlab=params$xlab, ylab=y_var_long, group='plot_DO_preds') %>%
+          dygraphs::dySeries('mod', drawPoints = FALSE, label=paste0("Modeled ", y_var_long), color=y_var_col[1]) %>%
+          dygraphs::dySeries('obs', drawPoints = TRUE, strokeWidth=0, label=paste0("Observed ", y_var_long), color=y_var_col[2]) %>%
+          dygraphs::dyAxis('y', valueRange=c(ymin,ymax)) %>%
+          dygraphs::dyOptions(colorSaturation=1) %>%
+          dygraphs::dyLegend(labelsSeparateLines = TRUE, width=300) %>%
+          dygraphs::dyRangeSelector(height = 20) %>%
+          print()
+      })
+      invisible()
+    }
+  )
 }
--- tests/testthat/test-metab_night.R
+++ tests/testthat/test-metab_night.R
@@ -25,8 +25,8 @@ test_that("metab_night predictions (predict_metab, predict_DO) make sense", {
   DO_preds <- predict_DO(mm)
   DO_preds_Aug24 <- dplyr::filter(DO_preds, local.date == "2012-08-24")
   expect_true(all(abs(DO_preds_Aug24$DO.obs - DO_preds_Aug24$DO.mod) < 0.15), "DO.mod tracks DO.obs with not too much error")
-  # plot_DO_preds(DO_preds, plot_as="conc")
-  # plot_DO_preds(DO_preds, plot_as="pctsat")
+  # plot_DO_preds(DO_preds, y_var="conc")
+  # plot_DO_preds(DO_preds, y_var="pctsat")
   
 })
 
