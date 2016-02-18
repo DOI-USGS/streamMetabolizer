34f23c59b89f41fd1dee87853f6ec5765124e3ff
Alison Appling <aappling@usgs.gov>
2016-02-17 19:15:56 -0600

    model by ply w solar.time, solar.date, solar_date

--- R/mm_model_by_ply.R
+++ R/mm_model_by_ply.R
@@ -6,13 +6,13 @@
 #' row-bound together into a single data.frame containing results from all days.
 #' 
 #' @param model_fun the function to apply to each data ply. This function should
-#'   accept the arguments \code{c(data, data_daily, ..., day_start, day_end, local_date)}
+#'   accept the arguments \code{c(data, data_daily, ..., day_start, day_end, solar_date)}
 #'   where \code{data_daily} is \code{NULL} when the \code{data_daily} argument
 #'   to \code{mm_model_by_ply} is missing or \code{NULL}
 #' @param data required. the data.frame containing all relevant, validated 
-#'   modeling data. The local.time column must be present.
+#'   modeling data. The solar.time column must be present.
 #' @param data_daily optional. a data.frame containing inputs with a daily 
-#'   timestep. The local.date column must be present.
+#'   timestep. The solar.date column must be present.
 #' @inheritParams mm_model_by_ply_prototype
 #' @param ... other args to be passed through mm_model_by_ply to model_fun
 #' @return a data.frame of model results
@@ -23,29 +23,29 @@ mm_model_by_ply <- function(model_fun, data, data_daily, day_start, day_end, ...
   # each date (day_start to day_end, which may be != 24). store this labeling in
   # two additional columns (odd.- and even.- date.groups)
   data.plys <- as.data.frame(v(data))
-  if(any(is.na(data.plys$local.time))) stop("no values in local.time may be NA")
-  data.plys$local.date <- format(data.plys$local.time, "%Y-%m-%d")
-  data.plys$hour <- 24*(convert_date_to_doyhr(data.plys$local.time) %% 1)
+  if(any(is.na(data.plys$solar.time))) stop("no values in solar.time may be NA")
+  data.plys$solar.date <- format(data.plys$solar.time, "%Y-%m-%d")
+  data.plys$hour <- 24*(convert_date_to_doyhr(data.plys$solar.time) %% 1)
   
-  unique.dates <- unique(as.character(data.plys$local.date))
+  unique.dates <- unique(as.character(data.plys$solar.date))
   odd.dates <- unique.dates[which(seq_along(unique.dates) %% 2 == 1)]
   even.dates <- unique.dates[which(seq_along(unique.dates) %% 2 == 0)]
   
   # add a tiny fudge factor to the start and end bounds so we don't exclude
   # times that are essentially equal to day_start or day_end
-  day_start <- day_start - 0.0001
-  day_end <- day_end + 0.0001
+  day_start <- day_start - 1e-10
+  day_end <- day_end + 1e-10
   
   dt.NA <- as.character(NA)
   for(dt in seq_along(unique.dates)) {
     dt.today <- unique.dates[dt]
     dt.yesterday <- if(dt>1) unique.dates[dt-1] else dt.NA
     dt.tomorrow <- if(dt<length(unique.dates)) unique.dates[dt+1] else dt.NA
-    hr <- data.plys[data.plys$local.date == dt.today, 'hour']
+    hr <- data.plys[data.plys$solar.date == dt.today, 'hour']
     primary.date <- c(dt.today, dt.NA)[ifelse(hr >= day_start & hr <= day_end, 1, 2)]
     secondary.date <- c(dt.yesterday, dt.NA, dt.tomorrow)[ifelse(hr <= (day_end - 24), 1, ifelse(hr < (24 + day_start), 2, 3))]
-    data.plys[data.plys$local.date == dt.today, 'odd.date.group'] <- if(dt.today %in% odd.dates) primary.date else secondary.date
-    data.plys[data.plys$local.date == dt.today, 'even.date.group'] <- if(dt.today %in% even.dates) primary.date else secondary.date
+    data.plys[data.plys$solar.date == dt.today, 'odd.date.group'] <- if(dt.today %in% odd.dates) primary.date else secondary.date
+    data.plys[data.plys$solar.date == dt.today, 'even.date.group'] <- if(dt.today %in% even.dates) primary.date else secondary.date
   } 
   
   # Estimate daily metabolism for each ply of the data, using if-else in the
@@ -57,11 +57,11 @@ mm_model_by_ply <- function(model_fun, data, data_daily, day_start, day_end, ...
       which(data.plys$even.date.group == dt)
     }
     out <- model_fun(
-      data_ply=data.plys[ply,!(names(data.plys) %in% c('local.date','hour','odd.date.group','even.date.group'))], 
-      data_daily_ply=if(!is.null(data_daily)) data_daily[data_daily$local.date == dt,] else NULL,
-      day_start=day_start, day_end=day_end, local_date=as.Date(dt, tz=tz(dt)),
+      data_ply=data.plys[ply,!(names(data.plys) %in% c('solar.date','hour','odd.date.group','even.date.group'))], 
+      data_daily_ply=if(!is.null(data_daily)) data_daily[data_daily$solar.date == dt,] else NULL,
+      day_start=day_start, day_end=day_end, solar_date=as.Date(dt, tz=tz(dt)),
       ...)
-    if(is.null(out) || nrow(out)==0) NULL else data.frame(local.date=as.Date(dt, tz=tz(dt)), out)
+    if(is.null(out) || nrow(out)==0) NULL else data.frame(solar.date=as.Date(dt, tz=tz(dt)), out)
   })
   # combine the 1-row dfs, making sure to start with a 0-row df that represents 
   # the dfs with the most columns to keep the column ordering consistent across 
@@ -70,7 +70,7 @@ mm_model_by_ply <- function(model_fun, data, data_daily, day_start, day_end, ...
   count_cols <- function(out) { if(is.null(out)) 0 else ncol(out) }
   example_choice <- which.max(sapply(out_list, count_cols))
   if(length(example_choice) == 0) {
-    data.frame(local.date=as.Date(NA)) 
+    data.frame(solar.date=as.Date(NA)) 
   } else {
     out_example <- out_list[[example_choice]][FALSE,]
     bind_rows(c(list(out_example), out_list)) %>% as.data.frame() 
--- R/mm_model_by_ply_prototype.R
+++ R/mm_model_by_ply_prototype.R
@@ -8,7 +8,7 @@
 #'   for a single ply of data. (1 ply ~= 1 date, although the day length has
 #'   been specified by day_start and day_end and may not be exactly 24 hours)
 #' @param data_daily_ply NULL or a data.frame containing inputs with a daily 
-#'   timestep. The local.date column must be present.
+#'   timestep. The solar.date column must be present.
 #' @param ... other args that were passed untouched from the function calling 
 #'   mm_model_by_ply, through mm_model_by_ply, and finally to this function.
 #' @param day_start start time of a day's data in number of hours from the 
@@ -21,9 +21,9 @@
 #'   describing 2006-06-26 end at 2006-06-27 06:00, or at the last observation 
 #'   time that occurs before that time if day_end doesn't fall exactly on an 
 #'   observation time.
-#' @param local_date the modal date of this ply of data and data_daily, and the 
+#' @param solar_date the modal date of this ply of data and data_daily, and the 
 #'   date by which this ply should be referred
 #' @export
-mm_model_by_ply_prototype <- function(data_ply, data_daily_ply, ..., day_start, day_end, local_date) {
+mm_model_by_ply_prototype <- function(data_ply, data_daily_ply, ..., day_start, day_end, solar_date) {
   message("this function does nothing and should only be called for testing")
 }
