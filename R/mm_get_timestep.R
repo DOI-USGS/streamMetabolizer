df221140256c70faa8313f7856a46ed7aac7d960
Alison Appling <aappling@usgs.gov>
2016-03-25 00:31:10 -0500

    'modal' option to mm_get_timestep

--- R/mm_get_timestep.R
+++ R/mm_get_timestep.R
@@ -13,24 +13,29 @@
 #'   (and thus 1/(24*60*60) is one second).
 #' @importFrom unitted v
 #' @examples {
-#' datetimes <- Sys.time()+ as.difftime(c(0,304,600,900.2,1200), units='secs')
+#' datetimes <- Sys.time()+ as.difftime(c(0,304,600,900.2,1200,1500,1800), units='secs')
 #' mm_get_timestep(datetimes, 'unique', tol=1/(24*60*60))
 #' mm_get_timestep(datetimes, 'unique', tol=5/(24*60*60))
 #' mm_get_timestep(datetimes, 'unique', tol=10/(24*60*60))
 #' mm_get_timestep(datetimes, 'unique', tol=300/(24*60*60))
 #' mm_get_timestep(datetimes, 'mean')
 #' mm_get_timestep(datetimes, 'mean', require_unique=TRUE, tol=300/(24*60*60))
+#' datetimes <- Sys.time()+ as.difftime(c(-1,0,2,4,5,6,8,10), units='days')
+#' mm_get_timestep(datetimes, 'modal')
 #' mm_get_timestep(c(), 'mean')
 #' mm_get_timestep(c(), 'unique')
+#' mm_get_timestep(c(), 'modal')
 #' \dontrun{
-#' mm_get_timestep(datetimes, 'mean', require_unique=TRUE, tol=1/(24*60*60)) # breaks
-#' mm_get_timestep(datetimes, 'unique', tol=5/(24*60*60), require_unique=TRUE) # breaks
+#' # all of these should and do give errors:
+#' mm_get_timestep(datetimes, 'mean', require_unique=TRUE, tol=1/(24*60*60))
+#' mm_get_timestep(datetimes, 'unique', tol=5/(24*60*60), require_unique=TRUE)
 #' mm_get_timestep(c(), 'mean', require_unique=TRUE)
 #' mm_get_timestep(c(), 'unique', require_unique=TRUE)
+#' mm_get_timestep(c(), 'modal', require_unique=TRUE)
 #' }
 #' }
 #' @export
-mm_get_timestep <- function(datetimes, format=c('mean','unique'), require_unique=FALSE, tol=60/(24*60*60)) {
+mm_get_timestep <- function(datetimes, format=c('mean','unique','modal'), require_unique=FALSE, tol=60/(24*60*60)) {
   timesteps <- as.numeric(diff(v(datetimes)), units="days")
   timestep <- switch(
     match.arg(format),
@@ -52,6 +57,20 @@ mm_get_timestep <- function(datetimes, format=c('mean','unique'), require_unique
       if(require_unique == TRUE && length(sufficiently_unique) != 1)
         stop('!=1 unique timestep')
       sufficiently_unique
+    },
+    modal = {
+      all_unique <- sort(unique(timesteps))
+      tbl_unique <- sapply(all_unique, function(tstep) sum(timesteps == tstep), USE.NAMES=FALSE)
+      sufficiently_unique <- mm_get_timestep(
+        datetimes=cumsum(c(0,all_unique)), format='unique', require_unique=require_unique, tol=tol)
+      round_unique <- approx(sufficiently_unique, sufficiently_unique, xout=all_unique, method='constant', rule=2)$y
+      tbl_suff_unique <- sapply(sufficiently_unique, function(ru) sum(tbl_unique[which(round_unique == ru)]))
+      all_suff_unique <- sapply(sufficiently_unique, function(ru) {
+        matches <- which(round_unique == ru)
+        best_match <- matches[which.max(tbl_unique[matches])]
+        all_unique[best_match]
+      })
+      all_suff_unique[which.max(tbl_suff_unique)]
     })
   timestep
 }
