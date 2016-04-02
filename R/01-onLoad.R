54828f21be6311afe6b5927738ac08a4d0da42ad
Alison Appling <aappling@usgs.gov>
2016-04-02 11:05:46 -0500

    on attach, check for updates on GRAN/GitHub

--- R/01-onLoad.R
+++ R/01-onLoad.R
@@ -2,6 +2,42 @@
 #'@keywords internal
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("This information is preliminary or provisional and is subject to revision. It is being provided to meet the need for timely best science. The information has not received final approval by the U.S. Geological Survey (USGS) and is provided on the condition that neither the USGS nor the U.S. Government shall be held liable for any damages resulting from the authorized or unauthorized use of the information. Although this software program has been used by the USGS, no warranty, expressed or implied, is made by the USGS or the U.S. Government as to the accuracy and functioning of the program and related program material nor shall the fact of distribution constitute any such warranty, and no responsibility is assumed by the USGS in connection therewith.")
+  
+  # Check whether this package is up to date
+  
+  GRAN_update_code <- paste0(
+    '  update.packages(oldPkgs=c("streamMetabolizer","unitted"),\n',
+    '    dependencies=TRUE, repos=c("http://owi.usgs.gov/R", "https://cran.rstudio.com"))')
+  github_owner <- 'USGS-R'
+  github_branch <- 'develop'
+  github_pkg_ref <- paste0(github_owner,'/',pkgname,'@',github_branch)
+  github_update_code <- paste0(
+    '  devtools::install_github("',github_pkg_ref,'")')
+  
+  GRAN_pkg <- available.packages(contrib.url("http://owi.usgs.gov/R"))
+  GRAN_version <- package_version(GRAN_pkg[[pkgname, 'Version']])
+  local_version <- packageVersion(pkgname)
+  if(local_version < GRAN_version) {
+    packageStartupMessage(
+      'Time to update to ', pkgname, ' version ', GRAN_version, '! You have ', local_version, '. Get stable updates with\n',
+      GRAN_update_code)
+  }
+  
+  if(requireNamespace('devtools', quietly=TRUE)) {
+    tryCatch({
+      github_ref <- devtools:::github_resolve_ref(
+        devtools::github_release(), 
+        devtools:::parse_git_repo(github_pkg_ref))$ref
+      github_version <- package_version(gsub('v', '', github_ref))
+      if(local_version < github_version) {
+        packageStartupMessage(
+          'New development version of ', pkgname, ' (', github_version, ') is ready! You have ', local_version, '. Get dev updates with\n',
+          github_update_code)
+      }
+    }, error=function(e) {
+      packageStartupMessage("Can't check GitHub for new package versions just now. We'll try again next time.")
+    })
+  }
 }
 library(methods)
 
