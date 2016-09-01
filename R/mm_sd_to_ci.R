c82e5acafeb88be6a3fbd5ab6cb171619b0219ca
Alison Appling <aappling@usgs.gov>
2016-09-01 16:36:39 -0700

    sd_to_ci robust to empty sd.cols and/or var.cols

--- R/mm_sd_to_ci.R
+++ R/mm_sd_to_ci.R
@@ -14,13 +14,13 @@ mm_sd_to_ci <- function(data, alpha=0.05) {
   # identify the pairs of columns
   sd.cols <- grep('\\.sd$', names(data), value=TRUE)
   var.cols <- substring(sd.cols, 1, nchar(sd.cols)-3) %>% { .[. %in% names(data)] } # could be fewer than sd.cols
-  sd.cols <- paste0(var.cols, '.sd') # only include those cols that both end in '.sd' and have a paired var.cols value
+  if(length(sd.cols) > 0) sd.cols <- paste0(var.cols, '.sd') # only include those cols that both end in '.sd' and have a paired var.cols value
   sd.pos <- match(sd.cols, names(data))
   
   # replace each var.sd col with var.lower and var.upper cols
   crit <- -qnorm(alpha/2)
   dat.list <- as.list(data)
-  for(i in length(var.cols):1) {
+  for(i in rev(seq_along(var.cols))) {
     est <- dat.list[[var.cols[i]]]
     sd <- dat.list[[sd.cols[i]]]
     dat.list <- append(
