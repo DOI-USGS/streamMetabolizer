871b5eaab9839b411ff0925978b54c634578d929
Alison Appling <aappling@usgs.gov>
2016-12-22 20:06:13 -0700

    fix example failures/slowness

--- R/metab_model.get_param_names.R
+++ R/metab_model.get_param_names.R
@@ -34,11 +34,9 @@ get_param_names.character <- function(metab_model) {
 #' @examples 
 #' 
 #' # or pass in a metab_model object:
-#' dat <- data_metab()
+#' dat <- data_metab('1','30')
 #' get_param_names(metab(specs(mm_name('mle', ER_fun='q10temp')), data=dat))
-#' get_param_names(metab(specs('bayes'), data=dat))
 #' get_param_names(metab(specs('night'), data=dat))
-#' get_param_names(metab(specs('Kmodel'), data=dat))
 #' get_param_names(metab(specs('sim'), data=dat))
 get_param_names.metab_model <- function(metab_model) {
   get_param_names(get_specs(metab_model)$model_name)
